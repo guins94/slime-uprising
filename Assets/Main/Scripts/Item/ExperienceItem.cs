fb1da1142925e5ccdbb25315553b761dfe9e360c
guins94 <guins94@hotmail.com>
2023-04-21 21:59:30 -0300

    Update ExperienceItem.cs

--- Assets/Main/Scripts/Item/ExperienceItem.cs
+++ Assets/Main/Scripts/Item/ExperienceItem.cs
@@ -3,13 +3,15 @@ using UnityEngine;
 
 public class ExperienceItem : Item
 {
-    private int experienceGained = 1;
+    public int experienceGained = 1;
 
     public override IEnumerator ItemEffect()
     {
         if (GameManager.Player != null)
         {
             GameManager.Player.ExperienceSystem.AddExperience(experienceGained);
+            GameManager.EnemySpawn.ExperienceItemCollected();
+            StartCoroutine(ItemDelete());
         }
         yield return null;
     }
