1a7dbc0f43c49eed731ec74118fe586be542536a
guins94 <guins94@hotmail.com>
2023-04-21 21:58:26 -0300

    Max Experience Item Created

---
+++ Assets/Main/Scripts/Item/MaxExperienceItem.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using UnityEngine;
+
+public class MaxExperienceItem : ExperienceItem
+{
+    public void AddExperience()
+    {
+        experienceGained = experienceGained + 1;
+    }
+}
