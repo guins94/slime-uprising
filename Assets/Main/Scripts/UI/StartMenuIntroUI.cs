c5cbce161d7d395b327e1396213371597f7e6327
guins94 <guins94@hotmail.com>
2023-05-31 17:30:46 -0300

    Start Menu Script Created

---
+++ Assets/Main/Scripts/UI/StartMenuIntroUI.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuIntroUI : MonoBehaviour
+{
+    [Header("Canvas References")]
+    [SerializeField] Canvas StartUpMenuCanvas;
+}
