3965a1090724b3a661c3c9c21f23013b3ddd1c80
guins94 <guins94@hotmail.com>
2023-06-01 00:29:49 -0300

    Update StartMenuIntroUI.cs

--- Assets/Main/Scripts/UI/StartMenuIntroUI.cs
+++ Assets/Main/Scripts/UI/StartMenuIntroUI.cs
@@ -1,9 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class StartMenuIntroUI : MonoBehaviour
 {
-    [Header("Canvas References")]
+    [Header("Main Canvas References")]
     [SerializeField] Canvas StartUpMenuCanvas;
+
+    [Header("Config GameObject References")]
+    [SerializeField] GameObject ConfigCanvas;
+
+    [Header("Grid References")]
+    [SerializeField] GameObject GameGrid;
+    [SerializeField] GameObject HolderCanvasGameObject;
+
+    [Header("Button References")]
+    [SerializeField] Button PlayGameButton;
+    [SerializeField] Button ConfigGameButton;
+    [SerializeField] Button QuitGameButton;
+
+    void Start()
+    {
+        // Activate Grid and Holder Using Timeline Activate 
+        GameGrid.SetActive(true);
+        HolderCanvasGameObject.SetActive(true);
+
+        // Starts Listenning to Button Clicks
+        PlayGameButton.onClick.AddListener(SceneLoading);
+        ConfigGameButton.onClick.AddListener(ActivateConfigGameCanvas);
+        QuitGameButton.onClick.AddListener(QuitGame);
+    }
+
+    private void SceneLoading()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    private void ActivateConfigGameCanvas()
+    {
+        ConfigCanvas.SetActive(true);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
 }
