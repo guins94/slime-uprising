51c102462653192c35dedc295edcab120157bfce
guins94 <guins94@hotmail.com>
2023-06-08 20:00:20 -0300

    Key Item Related Scripts and Prefab Created

---
+++ Assets/Main/Scripts/Item/Keys/KeyItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyItem : Item
+{
+    public override IEnumerator ItemEffect()
+    {
+        yield return null;
+    }
+}
